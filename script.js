5b56c497bfaafd6f8ec7468f9503ce24f0d4a278
Ethan Irvine <edog.irvine@gmail.com>
2019-12-16 14:24:14 -0700

    created a text area for the scheadule

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ let currentDay = moment().format('MMMM Do YYYY');
 let currentTime = moment().format('h:mm:ss a');
 console.log(currentTime);
 
-
 $(document).ready(function(){
     $('.currentDay').text(currentDay);
 });
+
+let time_slots = [];
+let start_time = 6;
+let end_time = 18;
+
+
+for( let i = start_time ; i <= end_time ; i++ ){
+	let suffix = 'AM';
+    let hour = i;
+    if( i >= 12 ){
+        suffix = 'PM';
+    } if( i >= 13 ){
+        hour = i - 12;
+    }
+    
+    time_slots[i] = hour + ':00 ' + suffix;
+}
+
+time_slots.forEach( function( item, index){
+	$( '#calendar').append( '<li class="list-group-item time-slot" id="time-slot-' + index + '">' + item +': <input type="text" id="input time-slot-appt-' + index + '"></input></li>' );
+	console.log ( "hour: " + index + ", pretty display: " + item );
+} );
